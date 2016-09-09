7be3669e863b0ae4493bc4d91c497149bff0abdf
Chew Chee Ming <cheeming@infinite-code.com>
2016-09-10 00:29:45 +0800

    Added uninstall/cleanup procedure
     * Added gunicorn as the default entrypoint

--- docker/skeleton/gen-dockerfiles-from-template.py
+++ docker/skeleton/gen-dockerfiles-from-template.py
@@ -12,6 +12,7 @@ def run():
                 open(filename_dockerfile, 'w') as dest:
             for line in src:
                 if keyword_req in line:
+                    uninstall_dependencies = []
                     if not setup_done:
                         dest.write('RUN mkdir -p {}\n'.format(
                             dir_pip_packages))
@@ -34,15 +35,22 @@ def run():
                             if '==' in package:
                                 package_name = package.split('==')[0]
                             if package_name in deps_map:
+                                dd = deps_map[package_name]
                                 dest.write(
-                                    'RUN apt-get install -y {}\n'.format(
-                                        deps_map[package_name]))
+                                    'RUN apt-get install -y {}\n'.format(dd))
+                                for d in dd.split():
+                                    if d.endswith('-dev') or d in ['gcc']:
+                                        uninstall_dependencies.append(d)
                             dest.write('{} download -d {} {}\n'.format(
                                 dockerfile_cmd, dir_pip_packages, package))
                             dest.write(
                                 '{} install --find-links file://{} {}\n'
                                 .format(dockerfile_cmd, dir_pip_packages,
                                         package))
+                    for d in uninstall_dependencies:
+                        dest.write(
+                            'RUN apt-get remove -y {}\n'.format(d))
+
                 else:
                     dest.write(line)
 
